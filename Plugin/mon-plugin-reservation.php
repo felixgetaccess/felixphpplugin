291b6a75b2d3d5ffdd589185e5a967dbcd44fd71
Felix Freyss <felix.freyss@gmail.com>
2023-12-19 11:33:04 +0100

    MAJ pour spliter les crénaux

--- Plugin/mon-plugin-reservation.php
+++ Plugin/mon-plugin-reservation.php
@@ -5,6 +5,14 @@ function afficher_formulaire_reservation() {
     <form action="<?php echo esc_url( $_SERVER['REQUEST_URI'] ); ?>" method="post">
         <label for="date-reservation">Date de réservation:</label>
         <input type="date" id="date-reservation" name="date_reservation">
+        
+        <label for="creneau-reservation">Créneau horaire:</label>
+        <select id="creneau-reservation" name="creneau_reservation">
+            <option value="9h-10h">9h-10h</option>
+            <option value="10h-11h">10h-11h</option>
+            <!-- Ajoutez d'autres créneaux horaires selon vos besoins -->
+        </select>
+        
         <input type="submit" name="submit_reservation" value="Réserver">
     </form>
     <?php
@@ -16,16 +24,17 @@ add_shortcode('formulaire_reservation', 'afficher_formulaire_reservation');
 function traiter_reservation() {
     if (isset($_POST['submit_reservation']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
         $date_reservation = sanitize_text_field($_POST['date_reservation']);
+        $creneau_reservation = sanitize_text_field($_POST['creneau_reservation']);
         
-        if (!empty($date_reservation)) {
+        if (!empty($date_reservation) && !empty($creneau_reservation)) {
             // Envoyer un e-mail aux administrateurs
             $to = get_option('admin_email');
             $subject = 'Nouvelle réservation';
-            $message = 'Une nouvelle réservation a été faite pour la date: ' . $date_reservation;
+            $message = 'Une nouvelle réservation a été faite pour la date: ' . $date_reservation . ' au créneau ' . $creneau_reservation;
             wp_mail($to, $subject, $message);
 
             // Message de confirmation pour l'utilisateur
-            echo '<div>Votre réservation pour le ' . esc_html($date_reservation) . ' a été envoyée.</div>';
+            echo '<div>Votre réservation pour le ' . esc_html($date_reservation) . ' au créneau ' . esc_html($creneau_reservation) . ' a été envoyée.</div>';
         }
     }
 }
